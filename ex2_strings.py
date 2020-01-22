40db35f4e69b24668a3353cea91fb0e082cbe444
Thalyta Alencar <thalycomp@gmail.com>
2020-01-22 14:44:02 -0300

    melhorando o programa c o metodo count

--- ex2_strings.py
+++ ex2_strings.py
@@ -3,13 +3,10 @@ Exercício 3: Encapsule esse código em uma função chamada contagem,
 e generalize para que ela aceite a string e a letra como argumentos.
 
 """
-try:
-    palavra = input("Palavra > ")
-    letter = input("Letra > ")
+palavra = input("Palavra > ")
+letter = input("Letra > ")
 
-    def contagem(palavra, letra):
-        print(palavra.count(letra))
+def contagem(palavra, letra):
+    print(palavra.count(letra))
 
-    contagem(palavra, letter)
-except:
-    input("Valor inválido! ")
+contagem(palavra, letter)
