29923ddb33f25add8adbada1a6189f0c0914fff3
Thalyta Alencar <thalycomp@gmail.com>
2020-01-23 09:18:44 -0300

    acrescentando mais informações

--- ex2_arquivos.py
+++ ex2_arquivos.py
@@ -2,6 +2,11 @@
 Exercício 2: Escreva um programa que solicite um arquivo e então leia
 ele e procure por linhas da forma:
 X-DSPAM-Confidence: 0.8475
+Quando encontrar uma linha que inicie com “X-DSPAM-Confidence:”
+separe a linha do texto para extrair o número de ponto flutuante que
+ela contém. Conte essas linhas e então compute o total de valores de
+Confiança de Spam nelas. Quando chegar no fim do arquivo, mostre a
+média de Confiança de Spam.
 
 """
 try:
@@ -12,5 +17,8 @@ except:
     exit()
 
 for linha in arquivo:
-    if linha.startswith("X-DSPAM-Confidence: 0.8475"):
-        print(linha)
+    if linha.startswith("X-DSPAM-Confidence:"):
+        inicio = linha.find(" ")
+        nv_ = linha[inicio+1::]
+        print(nv_)
+
