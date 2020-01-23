fb0b2c618f864b0030f1290d1bd6d6986924a3c2
Thalyta Alencar <thalycomp@gmail.com>
2020-01-23 09:31:42 -0300

    programa finalizado

--- ex2_arquivos.py
+++ ex2_arquivos.py
@@ -15,10 +15,21 @@ try:
 except:
     print("Erro. Arquivo não encontrado. ")
     exit()
+cont = 0
+media = 0
 
 for linha in arquivo:
+
     if linha.startswith("X-DSPAM-Confidence:"):
+        cont += 1
         inicio = linha.find(" ")
         nv_ = linha[inicio+1::]
+        nv_ = float(nv_)
+        media += nv_
+
         print(nv_)
+
+media = media / cont
+print(f"Media de confiança de Spam: {media:.4f}")
+
 
